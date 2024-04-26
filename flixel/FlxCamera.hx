1a243dcc416d41546545180153055c097175cc4d
CharlesCatYT <sptubea@gmail.com>
2024-04-25 21:30:00 -0400

    backporting from newer flixel commits part 3

--- flixel/FlxCamera.hx
+++ flixel/FlxCamera.hx
@@ -1752,6 +1752,14 @@ class FlxCamera extends FlxBasic
 	}
 
 	/**
+	 * Gets the filter array applied to the camera.
+	 */
+	public function getFilters()
+	{
+		return _filters;
+	}
+
+	/**
 	 * Copy the bounds, focus object, and `deadzone` info from an existing camera.
 	 *
 	 * @param   Camera  The camera you want to copy from.
--- flixel/system/FlxAssets.hx
+++ flixel/system/FlxAssets.hx
@@ -258,7 +258,7 @@ class FlxAssets
 	public static inline function getBitmapData(id:String):BitmapData
 	{
 		if (Assets.exists(id))
-			return Assets.getBitmapData(id, false);
+			return Assets.getBitmapData(id, true);
 		FlxG.log.error('Could not find a BitmapData asset with ID \'$id\'.');
 		return null;
 	}
